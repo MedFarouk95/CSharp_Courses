8255bdac2fb7e3bcf6e7c520e82e4656453f3566
Fofo <Fofo@DESKTOP-JOOC7VU>
2016-10-23 14:28:40 +0100

    Add Comment

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //This is My first App:
+
             Console.WriteLine("Hello World..!");
             Console.ReadKey();
         }
